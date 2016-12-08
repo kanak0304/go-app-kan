7186c6bd30a144ea57f0a5cd45375082b480976d
Kan Nakamura <knakamura@pivotal.io>
2016-12-08 14:48:18 +0900

    commit 12/07 2:48PM

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/go-martini/martini"
+)
+
+func main() {
+	message := os.Getenv("MESSAGE")
+	if message == "" {
+		message = "Hello world v2"
+	}
+
+	m := martini.Classic()
+	m.Get("/", func() string {
+		fmt.Println(message)
+		return message
+	})
+	m.Run()
+}
